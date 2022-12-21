909df182622c2f3f66764d5acc85a7bcbc959057
Paige Jenkins <paigemjenkins28@gmail.com>
2022-12-21 00:46:13 -0500

    final changes

--- main.js
+++ main.js
@@ -1,62 +1,74 @@
+// Grid DOM elements
+const grid = document.querySelector('#grid');
+const newSize = document.getElementById('grid-size');
+
 // Global variables
-let selection = '';
-//DOM elements
-const grid = document.getElementById('grid'); // Grid
-const color = document.getElementById('color'); // User selection from color wheel
-const single = document.getElementById('single'); //single-color mode
-//const eraseBtn = document.getElementById('erase'); //erase button
-  // each "square" div
-// Event listener that assigns #color to 'selection'
-single.addEventListener('click', getUserSelection());
-//EL that assigns white to erase button
-/*raseBtn.addEventListener('click', erase());*/
-function getUserSelection() {
-    color.addEventListener("input", function(){
-        selection = color.value;
-    });
-}
+let draw = false; // Register if user is actively coloring
+let selection =''; // Color of pixel
 
-/*function erase() {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "white";
-} IDK wtf is going on here lord help em i thought i knew what i was doing for a second*/
+// Creates Grid and sets pixel size
+function createGrid(size) {
+    let area = size * size;
+    for (let i = 0; i <= area; i++) {
+        grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        const div = document.createElement("div");
+        div.classList.add("square");
+        grid.appendChild(div);
 
-function makeGrid(size) {
-    /*let squares = grid.querySelectorAll('div');
-    squares.forEach((div) => div.remove());*/
-
-    let sizeAmt = size * size;
-
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-    for(let i = 0; i < sizeAmt; i++) {
-        let square = document.createElement("div");
-        square.style.backgroundColor = "white";
-        grid.insertAdjacentElement("beforeend", square);
-        // mouseover event listener
-        square.addEventListener('mouseover', function(){
-            square.style.backgroundColor = selection;
+        // Mouseover and mousedown event listeners
+        div.addEventListener('mouseover', function() {
+            if(!draw) {
+                return;
+            }
+            div.style.backgroundColor = selection;
+        });
+        div.addEventListener('mousedown', function() {
+            div.style.backgroundColor = selection;
         });
     }
 }
 
-makeGrid(42); // **Need to make funciton that allows user to dynamically change grid size
+// Event listeners to only draw when mouse is clicked and to stop when it is released.
+grid.addEventListener("mousedown", function() {
+    draw = true;
+})
+grid.addEventListener("mouseup", function() {
+    draw = false;
+})
 
-/*grid.getElementsByClassName.gridTemplateColumns = "repeat(16, 1fr)";
-grid.getElementsByClassName.gridTemplateRows = "repeat(16, 1fr)";*/
-/*createGrid = () => {
-    for (let i = 0; i < 256; i++) {
-        const div = document.createElement("div");
-        div.classList.add("square");
-        grid.appendChild(div);
-    }
-};*/
+// Creates default grid of size 16x16 when page loads
+createGrid(16); 
 
+// Assigns new grid size value from user input
+function gridSize() { 
 
-
-// Display slider value
-let sliderValue = document.getElementById("slider-value");
-function displaySlider(val) {
-    sliderValue.textContent = `${val} x ${val}`;
+    grid.innerHTML = '';
+    createGrid(`${newSize.value}`);
 }
+
+// Event listener to change size of grid with user input
+newSize.addEventListener('input', gridSize); 
+
+
+// Color wheel and button DOM elements
+const color = document.getElementById('color') // User selection from color wheel
+const single = document.getElementById('single') // Color button
+const erase = document.getElementById('erase'); // Pixelated erase button
+const reset = document.getElementById('reset'); // Reset grid button
+
+// Assigns color wheel value to pixel
+function getUserSelection() {
+    selection = color.value;
+}
+
+// Restores original background color of grid
+function eraseGrid() {
+    selection = 'white';
+}
+
+// Button and color wheel event listeners
+color.addEventListener('input', getUserSelection); // Color wheel input
+single.addEventListener('click', getUserSelection); // Color button
+erase.addEventListener('click', eraseGrid); // Erase button
+reset.addEventListener('click', gridSize); // Reset button
